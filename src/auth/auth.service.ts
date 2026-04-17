4d056e348f4abb9469cdc1d9647a4d93a73c2ae0
unknown <jmario.pl15@gmail.com>
2026-04-17 09:56:42 -0600

    Primer commit

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,87 @@
+import { ConflictException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateUserDto } from './dto/create-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { LoginUserDto } from './dto/login-user.dto';
+
+@Injectable()
+export class AuthService {
+  // Para usar registrar en la base de datos
+  constructor(@InjectRepository(User) private repoUser: Repository<User>) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const {email, password} = createUserDto;
+
+    // 1.- Verificamos que no existe un usuario con el mismo correo
+    const emailExist = await this.repoUser.findOneBy({email});
+    if (emailExist) {
+      const error = {
+        "statusCode": 409,
+        "error": "Conflict",
+        "message": ["El email ya existe"]
+      }
+
+      //Si se cumple la condición, el usuario existe en la base de datos
+      throw new ConflictException(error);
+    }
+
+    // 2.- Encriptamos el password
+    const numRound = 10;
+    const hashPassword = await bcrypt.hash(password, numRound)
+
+    createUserDto.password = hashPassword;
+
+    // 3.- Y guardamos en base de datos
+    return this.repoUser.save(createUserDto);
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const {email, password} = loginUserDto;
+
+    // 1.- Verificar que el email existe
+    const emailExist = await this.repoUser.findOneBy({email});
+    if (!emailExist) {
+      const error = {
+        "statusCode": 404,
+        "error": "Not Found",
+        "message": ["El usuario no existe"]
+      }
+
+      //Si se cumple la condición, el usuario existe en la base de datos
+      throw new NotFoundException(error);
+    }
+
+    // 2.- Comparar contraseñas hasheadas
+    const matchPassword = await bcrypt.compare(password, emailExist.password)
+    if (!matchPassword) {
+      const error = {
+        "statusCode": 401,
+        "error": "Unauthorized",
+        "message": ["Contraseña incorrecta"]
+      }
+
+      throw new UnauthorizedException(error);
+    }
+
+    // 3.- Regresar el token JWT
+  }
+
+  findAll() {
+    return `This action returns all auth`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} auth`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} auth`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} auth`;
+  }
+}
