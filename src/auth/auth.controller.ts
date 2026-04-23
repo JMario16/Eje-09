622514df681bf19e4a557843bab5795b25b9453e
unknown <jmario.pl15@gmail.com>
2026-04-23 09:54:01 -0600

    Tercer commit

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
+import { AuthGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -21,28 +22,11 @@ export class AuthController {
   }
 
   // auth/profile - - Protegida
+  // Para ver el perfil de un usuario, se requiere su token
+
+  @UseGuards(AuthGuard)
   @Get('/profile')
-  profile() {
-    return "Estas viendo un perfil protejido por un Token valido"
-  }
-
-  @Get()
-  findAll() {
-    return this.authService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
+  profile(@Request() req) {
+    return "Estas viendo un perfil protejido por un Token valido del usuario: " + req.user.name;
   }
 }
